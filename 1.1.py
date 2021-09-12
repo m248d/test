3a17010775dea139a722aee7c9c2938ae6838e3f
m248d <86578450+m248d@users.noreply.github.com>
2021-09-13 00:08:16 +0200

    Create 1.1.py

---
+++ 1.1.py
@@ -0,0 +1,8 @@
+"""1.1.1 function 1 one human year is equivalent to 7 dog years"""
+def calculator_dog_age(human_years):
+   dog_years = human_years* 7
+   return dog_years
+## test the function - Calculate the age of the dog that corresponds to a human of age 35
+print(calculator_dog_age(35))
+
+1.1.2 ## function 2
